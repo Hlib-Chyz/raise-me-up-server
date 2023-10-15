96cad1f8f0357c469c706438551e6c64e69858cd
GlebChiz <gchyz@jsdaddy.io>
2023-10-15 19:54:35 +0300

    fix: minor

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ErrorFilter());
-  await app.listen(4200);
+  const port = process.env.port || 4200;
+  await app.listen(port);
 }
 bootstrap();
