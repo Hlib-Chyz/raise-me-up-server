3c6afb51849670a92f40d6f3824dbc3a4c4c9482
GlebChiz <gchyz@jsdaddy.io>
2023-10-16 08:38:49 +0300

    fix: minor

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { ErrorFilter } from 'src/shared/filters/error.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(1);
   const app = await NestFactory.create(AppModule);
+  console.log(2);
   app.useGlobalPipes(new ValidationPipe());
+  console.log(3);
   app.useGlobalFilters(new ErrorFilter());
+  console.log(4);
   const port = process.env.port || 4200;
+  console.log(5);
   await app.listen(port);
+  console.log(6);
 }
 bootstrap();
