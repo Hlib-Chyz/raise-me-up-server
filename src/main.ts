3aa9f67ea36e1c5a4f3c5785c74316c8c104524e
GlebChiz <gchyz@jsdaddy.io>
2023-10-16 14:05:14 +0300

    fix: add enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(8080);
